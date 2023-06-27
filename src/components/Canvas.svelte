f06419f1332526efd9150f6d3d9da015b59e6525
Angel <santoyox714@gmail.com>
2023-06-27 11:54:34 -0700

    add canvas resize

--- src/components/Canvas.svelte
+++ src/components/Canvas.svelte
@@ -13,10 +13,15 @@
   
 	let t, l
 	let lineWidth = 5 // Initial line width
+	let canvasWidth = 500 // Initial canvas width
+	let canvasHeight = 500 // Initial canvas height
   
 	onMount(() => {
 	  context = canvas.getContext('2d')
 	  context.lineWidth = lineWidth
+  
+	  canvas.setAttribute('width', canvasWidth)
+	  canvas.setAttribute('height', canvasHeight)
   
 	  handleSize()
 	})
@@ -27,7 +32,7 @@
   
 	const handleStart = ({ offsetX: x, offsetY: y }) => {
 	  if (color === background) {
-		context.clearRect(0, 0, width, height)
+		context.clearRect(0, 0, canvasWidth, canvasHeight)
 	  } else {
 		isDrawing = true
 		start = { x, y }
@@ -65,35 +70,51 @@
 		context.lineWidth = lineWidth;
 	  }
 	}
+  
+	function changeCanvasSize() {
+	  const newWidth = parseInt(prompt("Enter a new canvas width:"));
+	  const newHeight = parseInt(prompt("Enter a new canvas height:"));
+  
+	  if (!isNaN(newWidth) && !isNaN(newHeight)) {
+		canvasWidth = newWidth;
+		canvasHeight = newHeight;
+  
+		canvas.setAttribute('width', canvasWidth);
+		canvas.setAttribute('height', canvasHeight);
+  
+		handleSize();
+	  }
+	}
   </script>
   
   <svelte:window on:resize={handleSize} />
   
-
+  <canvas
+	{canvasWidth}
+	{canvasHeight}
+	style:background
+	bind:this={canvas}
+	on:mousedown={handleStart}
+	on:touchstart={e => {
+	  const { clientX, clientY } = e.touches[0]
+	  handleStart({
+		offsetX: clientX - l,
+		offsetY: clientY - t
+	  })
+	}}
+	on:mouseup={handleEnd}
+	on:touchend={handleEnd}
+	on:mouseleave={handleEnd}
+	on:mousemove={handleMove}
+	on:touchmove={e => {
+	  const { clientX, clientY } = e.touches[0]
+	  handleMove({
+		offsetX: clientX - l,
+		offsetY: clientY - t
+	  })
+	}}
+  />
   
-  <button on:click={changeLineWidth}>Change brush width</button>
-<canvas
-				{width}
-				{height}
-				style:background
-				bind:this={canvas} 
-				on:mousedown={handleStart}	
-				on:touchstart={e => {
-					const { clientX, clientY } = e.touches[0]
-					handleStart({
-						offsetX: clientX - l,
-						offsetY: clientY - t
-					})
-				}}	
-				on:mouseup={handleEnd}				
-				on:touchend={handleEnd}				
-				on:mouseleave={handleEnd}
-				on:mousemove={handleMove}
-				on:touchmove={e => {
-					const { clientX, clientY } = e.touches[0]
-					handleMove({
-						offsetX: clientX - l,
-						offsetY: clientY - t
-					})
-				}}
-				/>
+  <button on:click={changeLineWidth}>Change Line Width</button>
+  <button on:click={changeCanvasSize}>Change Canvas Size</button>
+  
