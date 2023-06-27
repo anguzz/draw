3816eb573b36e292da33d0edeed9bd1b15c03dca
Angel <santoyox714@gmail.com>
2023-06-27 12:34:46 -0700

    add footer

---
+++ src/components/Draw.svelte
@@ -0,0 +1,64 @@
+<script>
+    import Canvas from '../components/Canvas.svelte';
+    import Palette from '../components/Colors.svelte';
+
+	const colors = [
+        '#ff0000',
+        '#006600',
+        '#ffff00',
+        '#003d99',
+        '#000000',
+
+	]
+	const background = '#fff'
+
+	let color = colors[0]
+	const paletteColor = color
+</script>
+
+<main>
+	<Canvas 
+					{color} 
+					{background} />
+	<Palette 
+					 {paletteColor}
+					 {colors}
+					 {background}
+					 on:color="{({ detail }) => {
+							color = detail.color
+						}}"/>
+</main>
+
+<style>	
+	:global(body) {
+		margin: 0;
+		padding: 0;
+		min-height: 100vh;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background: #375082;
+	}
+	
+	:global(.visually-hidden:not(:focus):not(:active)) {
+		clip: rect(0 0 0 0);
+		clip-path: inset(50%);
+		height: 1px;
+		width: 1px;
+		overflow: hidden;
+		position: absolute;
+		white-space: nowrap;
+	}
+	
+	main {
+		max-width: 300px;
+		display: flex;
+		flex-direction: column;
+		gap: 0.5rem 0;
+	}
+	
+	main :global(canvas) {
+		align-self: center;
+	}
+	
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang='ts'>
+	
+	import Footer from '../components/Footer.svelte';
+</script>
+
+
+
+<slot />
+<Footer/>
