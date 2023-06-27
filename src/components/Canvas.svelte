442c9d6de9dc209783855e7fe62dbe3de56550a7
Angel <santoyox714@gmail.com>
2023-06-27 11:49:13 -0700

    add line width button

--- src/components/Canvas.svelte
+++ src/components/Canvas.svelte
@@ -1,63 +1,77 @@
 <script>
 	import { onMount } from 'svelte'
-	
+  
 	export let width = 500
 	export let height = 500
 	export let color = '#333'
 	export let background = '#fff'
-	
+  
 	let canvas
 	let context
 	let isDrawing
 	let start
-	
+  
 	let t, l
-	
-
-    //TODO implemenet button to change brush width
+	let lineWidth = 5 // Initial line width
+  
 	onMount(() => {
-		context = canvas.getContext('2d')
-		context.lineWidth = 5
-		
-		handleSize()
+	  context = canvas.getContext('2d')
+	  context.lineWidth = lineWidth
+  
+	  handleSize()
 	})
-	
-	$: if(context) {
-			context.strokeStyle = color
+  
+	$: if (context) {
+	  context.strokeStyle = color
 	}
-	
-	const handleStart = (({ offsetX: x, offsetY: y }) => { 
-		if(color === background) {
-			context.clearRect(0, 0, width, height)
-		} else {
-			isDrawing = true
-			start = { x, y }
-		}
-	})
-	
-	const handleEnd = () => { isDrawing = false }
-	const handleMove = (({ offsetX: x1, offsetY: y1 }) => {
-		if(!isDrawing) return
-		
-		const { x, y } = start
-		context.beginPath()
-		context.moveTo(x, y)
-		context.lineTo(x1, y1)
-		context.closePath()
-		context.stroke()
-		
-		start = { x: x1, y: y1 }
-	})
-	
+  
+	const handleStart = ({ offsetX: x, offsetY: y }) => {
+	  if (color === background) {
+		context.clearRect(0, 0, width, height)
+	  } else {
+		isDrawing = true
+		start = { x, y }
+	  }
+	}
+  
+	const handleEnd = () => {
+	  isDrawing = false
+	}
+  
+	const handleMove = ({ offsetX: x1, offsetY: y1 }) => {
+	  if (!isDrawing) return
+  
+	  const { x, y } = start
+	  context.beginPath()
+	  context.moveTo(x, y)
+	  context.lineTo(x1, y1)
+	  context.closePath()
+	  context.stroke()
+  
+	  start = { x: x1, y: y1 }
+	}
+  
 	const handleSize = () => {
-		const { top, left } = canvas.getBoundingClientRect()
-		t = top
-		l = left
+	  const { top, left } = canvas.getBoundingClientRect()
+	  t = top
+	  l = left
 	}
-</script>
+  
+	function changeLineWidth() {
+	  const newLineWidth = parseInt(prompt("Enter a new line width:"));
+  
+	  if (!isNaN(newLineWidth)) {
+		lineWidth = newLineWidth;
+		context.lineWidth = lineWidth;
+	  }
+	}
+  </script>
+  
+  <svelte:window on:resize={handleSize} />
+  
 
-<svelte:window on:resize={handleSize} />
-
+  
+  <button on:click={changeLineWidth}>Change Line Width</button>
 <canvas
 				{width}
 				{height}
